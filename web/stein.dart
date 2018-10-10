a7077715eed22490742126bcf8648a494de62b25
Max Zimmermann <max.zimcody212@gmx.de>
2018-10-10 14:02:10 +0200

    start

---
+++ web/stein.dart
@@ -0,0 +1,17 @@
+class Stein {
+
+  int elementnummer;
+  int designnummer;
+  String name;
+  int laenge;
+  int breite;
+  int hoehe;
+  String farbe;
+  int anzahl;
+
+  Stein(this.elementnummer, this.designnummer, this.name, this.laenge,
+      this.breite, this.hoehe, this.farbe, this.anzahl);
+
+
+
+}
