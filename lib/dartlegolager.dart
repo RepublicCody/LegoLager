1b1ec833253dabfe185aa17ec8b978ba27a807c4
Max Zimmermann <max.zimcody212@gmx.de>
2018-10-13 22:52:25 +0200

    Allgemeiner Struckturaufbau für vmc

---
+++ lib/dartlegolager.dart
@@ -0,0 +1,11 @@
+library legolager;
+
+import 'dart:html';
+import 'dart:convert';
+import 'dart:math';
+import 'dart:js';
+import 'dart:async';
+
+part 'src/model.dart';
+part 'src/controller.dart';
+part 'src/view.dart';
--- web/stein.dart
+++ web/stein.dart
@@ -12,6 +12,75 @@ class Stein {
   Stein(this.elementnummer, this.designnummer, this.name, this.laenge,
       this.breite, this.hoehe, this.farbe, this.anzahl);
 
+  int getElemtnummer(){
+    return this.elementnummer;
+  }
+  void setElemtnummer(int elementnummer){
+    this.elementnummer = elementnummer;
+  }
 
-String test = "";
+  int getDesignnummer(){
+    return this.designnummer;
+  }
+  void setDesignnummer(int designnummer){
+    this.designnummer = designnummer;
+  }
+
+  String getName(){
+    return this.name;
+  }
+  void setName(String name){
+    this.name = name;
+  }
+
+  int getlaenge(){
+    return this.laenge;
+  }
+  void setLaenge(int laenge){
+    this.laenge = laenge;
+  }
+
+  int getBreite(){
+    return this.breite;
+  }
+  void setbreite(int breite){
+    this.breite = breite;
+  }
+
+  int getHoehe(){
+    return this.hoehe;
+  }
+  void setHoehe(int hoehe){
+    this.hoehe = hoehe;
+  }
+
+  String getFarbe(){
+    return this.farbe;
+  }
+  void setFarbe(String farbe){
+    this.farbe = farbe;
+  }
+
+  int getAnzahl(){
+    return this.anzahl;
+  }
+  void setAnzahl(int anzahl){
+    this.anzahl = anzahl;
+  }
+
+  String getMasse(){
+    return this.laenge.toString() + "x" + this.breite.toString() + "x" + this.hoehe.toString();
+  }
+
+  void addAnzahl (int added){
+    this.anzahl += added;
+  }
+
+  void subtractAnzahl(int subtract){
+    this.anzahl -= subtract;
+  }
+
+  void deleteAnzahl (){
+    this.anzahl = 0;
+  }
 }
